5a183c8992e9d67bd4947ff53e77f5fac08859af
Sebastian Mahlow <Sebastian@Macbook-Pro-Sebastian.fritz.box>
2016-11-30 06:25:02 +0100

    first version

--- site.mk
+++ site.mk
@@ -1,37 +1,41 @@
 GLUON_SITE_PACKAGES := \
-  gluon-mesh-batman-adv-15 \
-  gluon-alfred \
-  gluon-respondd \
-  gluon-autoupdater \
-  gluon-config-mode-autoupdater \
-  gluon-config-mode-contact-info \
-  gluon-config-mode-core \
-  gluon-config-mode-geo-location \
-  gluon-config-mode-hostname \
-  gluon-config-mode-mesh-vpn \
-  gluon-ebtables-filter-multicast \
-  gluon-ebtables-filter-ra-dhcp \
-  gluon-ffbrb-autokey \
-  gluon-luci-theme \
-  gluon-luci-admin \
-  gluon-luci-autoupdater \
-  gluon-luci-portconfig \
-  gluon-luci-private-wifi \
-  gluon-luci-wifi-config \
-  gluon-next-node \
-  gluon-mesh-vpn-fastd \
-  gluon-radvd \
-  gluon-setup-mode \
-  gluon-status-page \
-  haveged \
-  iptables \
-  iwinfo
+	gluon-mesh-batman-adv-15 \
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iptables \
+	iwinfo
+
+
+DEFAULT_GLUON_RELEASE := 2016.2~$(GLUON_BRANCH)-$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
-GLUON_RELEASE ?= 2016.1.1~$(GLUON_BRANCH)-$(shell date '+%Y%m%d')
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 # Default priority for updates.
 GLUON_PRIORITY ?= 0
+
+# Region code required for some images; supported values: us eu
+GLUON_REGION ?= eu
 
 # Languages to include
 GLUON_LANGS ?= en de
